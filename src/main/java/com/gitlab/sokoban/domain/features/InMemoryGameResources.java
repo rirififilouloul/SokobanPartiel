54a838b208fb4531d2ebd3b96922209d47c24b83
RIRI <00.rirififiloulou.00@gmail.com>
2025-02-18 10:23:49 +0100

    2.2 in load waiting

---
+++ src/main/java/com/gitlab/sokoban/domain/features/InMemoryGameResources.java
@@ -0,0 +1,15 @@
+package com.gitlab.sokoban.domain.features;
+
+public class InMemoryGameResources implements GameResources {
+
+    //private final Sokoban sokoban;
+
+    public InMemoryGameResources() {
+        //this.sokoban = new Sokoban();
+    }
+
+    //public Sokoban get() {
+    //    return sokoban;
+    //}
+
+}
